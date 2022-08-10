406619936278f50286ae1212c6a9394864eb53af
Sungjin Mo <samuelmo78@gmail.com>
2022-08-10 15:58:28 -0600

    added dependency

---
+++ app/src/main/java/com/example/mvvm_compose/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.mvvm_compose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "Hey look some text")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Button(onClick = {}) {
+                    Text(text = "A BUTTON")
+                }
+            }
+        }
+    }
+}
